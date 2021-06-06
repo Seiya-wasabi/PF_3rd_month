5efeceaa951ff1aa2579ecabd66211af4449ddb8
Seiya-wasabi <se.sato614@gmail.com>
2021-06-06 07:53:18 +0000

    ルーティング、usersの退会フラグ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,26 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
-  devise_for :users
+  devise_for :admins, skip: :all
+    devise_scope :admin do
+      get 'admins/sign_in' => 'admins/sessions#new'
+      post 'admins/sign_in' => 'admins/sessions#create'
+      delete 'logout' => 'admins/sessions#destroy'
+    end
+      devise_for :admins, controllers: {
+        registrations: 'admins/registrations',
+        sessions: 'admins/sessions'
+      }
+
+    devise_for :users, skip: :all
+      devise_scope :user do
+        get 'users/sign_in' => 'users/sessions#new'
+        post 'users/sign_in' => 'users/sessions#create'
+        delete 'logout' => 'users/sessions#destroy'
+      end
+      devise_for :users, controllers: {
+        registrations: 'users/registrations',
+        sessions: 'users/sessions'
+      }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20210606073321_change_null_of_users.rb
@@ -0,0 +1,5 @@
+class ChangeNullOfUsers < ActiveRecord::Migration[5.2]
+  def change
+    change_column_null :users, :is_deleted, false
+  end
+end
