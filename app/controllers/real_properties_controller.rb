5889e4fb79257602d719496e7b9781ae4281be09
Seiya-wasabi <se.sato614@gmail.com>
2021-06-07 10:13:52 +0000

    コントローラー内修正

--- app/controllers/real_properties_controller.rb
+++ app/controllers/real_properties_controller.rb
@@ -1,10 +1,5 @@
 class RealPropertiesController < ApplicationController
-  include Scrapes
-
   def new
-    if params[:format] == "suumo"
-      @properties = set_real_property
-    end
     @property = Property.new
   end
 end
