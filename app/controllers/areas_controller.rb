d1d550a7a7fe17c121c6f824a52648fac3088853
Seiya-wasabi <se.sato614@gmail.com>
2021-06-19 05:38:53 +0000

    アイソレーション編集

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,4 +1,9 @@
 class AreasController < ApplicationController
   def new
+    @area = Area.new  
+  end
+  
+  def get_cities
+    render partial: 'select_city', locals: {prefecture_id: params[:prefecture_id]}
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_17_074936) do
+ActiveRecord::Schema.define(version: 2021_06_19_052824) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2021_06_17_074936) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "areas", force: :cascade do |t|
+    t.integer "prefecture_id"
+    t.integer "city_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "cities", force: :cascade do |t|
