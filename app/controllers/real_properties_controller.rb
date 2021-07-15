363703b229a8b6d0079746e15afb4138a2c2b126
Seiya-wasabi <se.sato614@gmail.com>
2021-07-15 14:15:47 +0000

    ランキング機能追加

--- app/controllers/real_properties_controller.rb
+++ app/controllers/real_properties_controller.rb
@@ -34,7 +34,8 @@ class RealPropertiesController < ApplicationController
   end
 
   def index
-      @rents = RealProperty.page(params[:page]).reverse_order
+    @rents = RealProperty.page(params[:page]).reverse_order
+    @all_ranks = Note.find(Like.group(:note_id).order('count(note_id) desc').limit(3).pluck(:note_id))
   end
 
   # require 'open-uri' # URLアクセス
