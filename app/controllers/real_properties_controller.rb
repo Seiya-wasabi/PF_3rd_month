c4468e50e6a3bce9eead3b48e97e3949742adb5a
Seiya-wasabi <se.sato614@gmail.com>
2021-07-17 07:47:59 +0000

    エリア選択まで再利用

--- app/controllers/real_properties_controller.rb
+++ app/controllers/real_properties_controller.rb
@@ -4,6 +4,8 @@ class RealPropertiesController < ApplicationController
 
   def new
     @url = RealProperty.new
+    @area = Area.new
+    @cities = City.where(prefecture_id:0)
   end
 
   def create
@@ -51,6 +53,10 @@ class RealPropertiesController < ApplicationController
   #       puts link.content
   #     end
   # end
+  def get_cities
+    @cities = City.where(prefecture_id: params[:prefecture_id])
+#    render partial: 'select_city', locals: {prefecture_id: params[:prefecture_id]}
+  end
 
     private
   # ストロングパラメータ
