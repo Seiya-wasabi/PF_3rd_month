a2cf84ed2a7fce2a48f0667e627f4aeed16de50d
Seiya-wasabi <se.sato614@gmail.com>
2021-07-22 14:42:47 +0000

    新規投稿　

--- app/controllers/user_lists_controller.rb
+++ app/controllers/user_lists_controller.rb
@@ -2,9 +2,14 @@ class UserListsController < ApplicationController
   def index
     @customers = Customer.page(params[:page])
   end
-  
-  
+
+
   def show
     @customer = Customer.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(content:params[:content])
+    @post.save
   end
 end
