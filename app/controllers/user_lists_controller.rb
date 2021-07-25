c3d96e98288e7b013c818192c11e97baa2c81dfe
Seiya-wasabi <se.sato614@gmail.com>
2021-07-25 14:21:15 +0000

    削除

--- app/controllers/user_lists_controller.rb
+++ app/controllers/user_lists_controller.rb
@@ -2,9 +2,4 @@ class UserListsController < ApplicationController
   def index
     @customers = Customer.page(params[:page])
   end
-
-
-  def show
-    @customer = Customer.find(params[:id])
-  end
 end
