2414eaa5846fe60323f81e9e57e67ed24f69daa4
Seiya-wasabi <se.sato614@gmail.com>
2021-07-27 14:23:09 +0000

    東京サーバーに入れない

--- app/controllers/user_lists_controller.rb
+++ app/controllers/user_lists_controller.rb
@@ -2,4 +2,10 @@ class UserListsController < ApplicationController
   def index
     @customers = Customer.page(params[:page])
   end
+
+  def create
+    @post = Post.new(content:params[:content])
+    @post.save
+  end
+
 end
