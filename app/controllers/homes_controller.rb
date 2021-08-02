fc0dcf96eef20a39c6ad7046d63067fd7b47915d
Seiya-wasabi <se.sato614@gmail.com>
2021-08-02 13:02:08 +0000

    update

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,11 @@
 class HomesController < ApplicationController
-end
-
+  def new
+    @area = Area.new
+    @cities = City.where(prefecture_id:0)
+  end
 
   def create
     @post = Post.new(content:params[:content])
     @post.save
-  en
+  end
+end
