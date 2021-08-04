0c70216d6831637dd4856aa7430dc808932b2a4b
Seiya-wasabi <se.sato614@gmail.com>
2021-08-04 11:45:48 +0000

    優先

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,13 +2,4 @@ class HomesController < ApplicationController
   def top
     
   end
-  def index
-    @customers = Customer.page(params[:page])
-  end
-
-  def new
-    @area = Area.new
-    @cities = City.where(prefecture_id:0)
-  end
-
 end
