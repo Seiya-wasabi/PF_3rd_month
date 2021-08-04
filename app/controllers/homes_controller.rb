ad100e00c634ea2377f9f8029bf3c898cb037ac6
Seiya-wasabi <se.sato614@gmail.com>
2021-08-04 11:45:28 +0000

    エリア選択を保存したい

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,5 +2,13 @@ class HomesController < ApplicationController
   def top
     
   end
+  def index
+    @customers = Customer.page(params[:page])
+  end
+
+  def new
+    @area = Area.new
+    @cities = City.where(prefecture_id:0)
+  end
 
 end
