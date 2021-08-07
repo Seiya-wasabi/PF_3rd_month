e33cffa26dda58104e9763094d086d722d884766
Seiya-wasabi <se.sato614@gmail.com>
2021-08-07 13:09:10 +0000

    女子バスケおめでとう！

--- app/controllers/real_properties_controller.rb
+++ app/controllers/real_properties_controller.rb
@@ -58,6 +58,13 @@ class RealPropertiesController < ApplicationController
 #    render partial: 'select_city', locals: {prefecture_id: params[:prefecture_id]}
   end
 
+  def new
+    @area = Area.new
+    @cities = City.where(prefecture_id:0)
+  end
+
+
+
     private
   # ストロングパラメータ
   def url_params
@@ -125,7 +132,7 @@ end #class全体
     @post = Post.new(content:params[:content])
     @post.save
   end
-  
+
   def show
     @customer = Customer.find(params[:id])
   end
--- app/controllers/user_lists_controller.rb
+++ app/controllers/user_lists_controller.rb
@@ -2,11 +2,4 @@ class UserListsController < ApplicationController
   def index
     @customers = Customer.page(params[:page])
   end
-
-  def new
-    @area = Area.new
-    @cities = City.where(prefecture_id:0)
-  end
-  
-  
 end
