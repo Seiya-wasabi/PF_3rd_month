1077a1b48bd2337ffb9e55fa3ce983e4a86671c8
Seiya-wasabi <se.sato614@gmail.com>
2021-08-12 10:47:32 +0000

    耐久

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,9 +1,10 @@
 class HomesController < ApplicationController
   def top
   end
-
-  def index
-    @rents = RealProperty.page(params[:page]).reverse_order
-    @all_ranks = Note.find(Like.group(:note_id).order('count(note_id) desc').limit(3).pluck(:note_id))
+  
+  def new
+    @url = RealProperty.new
+    @area = Area.new
+    @cities = City.where(prefecture_id:0)
   end
 end
