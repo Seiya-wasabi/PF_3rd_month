3e53e8cd570438da3bb8a1c716d929510e02dc67
Seiya-wasabi <se.sato614@gmail.com>
2021-08-14 12:42:13 +0000

    header追加

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
       registrations: 'admins/registrations'
     }
     
-      get   'inquiry'         => 'inquiry#index'     # 入力画面
+      get   'inquiry'         => 'inquiry#index', as: 'inquiry'   # 入力画面
       post  'inquiry/confirm' => 'inquiry#confirm'   # 確認画面
       post  'inquiry/thanks'  => 'inquiry#thanks'    # 送信完了画面
 
