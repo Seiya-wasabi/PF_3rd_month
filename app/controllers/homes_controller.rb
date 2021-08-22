c689d970a1b9a48e30851affb420cac8fa58f89e
Seiya-wasabi <se.sato614@gmail.com>
2021-08-22 13:06:01 +0000

    観戦

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -5,5 +5,17 @@ class HomesController < ApplicationController
     @customer = Customer.find(params[:id])
   end
   
+  def create
+    @scrape = Area.new.scrape
+    @scrapes = @scrape.all
+    redirect_to get_cities_areas
+  end
+
+
+  def get_cities
+    @cities = City.where(prefecture_id: params[:prefecture_id])
+#    render partial: 'select_city', locals: {prefecture_id: params[:prefecture_id]}
+  end
+  
 
 end
