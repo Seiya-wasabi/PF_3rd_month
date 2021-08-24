5b25236e13ce37af54ebd898b2d5dd4be7e60c78
Seiya-wasabi <se.sato614@gmail.com>
2021-08-24 14:16:25 +0000

    content

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,21 +1,4 @@
 class HomesController < ApplicationController
   def top
   end
-  def show
-    @customer = Customer.find(params[:id])
-  end
-  
-  def create
-    @scrape = Area.new.scrape
-    @scrapes = @scrape.all
-    redirect_to get_cities_areas
-  end
-
-
-  def get_cities
-    @cities = City.where(prefecture_id: params[:prefecture_id])
-#    render partial: 'select_city', locals: {prefecture_id: params[:prefecture_id]}
-  end
-  
-
 end
