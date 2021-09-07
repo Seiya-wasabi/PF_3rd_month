54ab78fb63c397c06f3481bc1314364736483622
Seiya-wasabi <se.sato614@gmail.com>
2021-09-07 14:00:39 +0000

    html

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,10 +1,5 @@
 class HomesController < ApplicationController
   def top
   end
-    
-  def new
-    @url = RealProperty.new
-    @area = Area.new
-    @cities = City.where(prefecture_id:0)
-  end
+  
 end
