5fa8ba53891c1aa1f4d64570466c44653ae10f58
Seiya-wasabi <se.sato614@gmail.com>
2021-09-16 14:30:53 +0000

    ace

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,10 +1,16 @@
 class HomesController < ApplicationController
   def top
   end
-  def index
-    # 入力画面を表示
-    @inquiry = Inquiry.new
-    render :action => 'index'
+  
+  def new
+    @area = Area.new
+    @cities = City.where(prefecture_id:0)
   end
+  
 
+  def create
+    @scrape = Area.new.scrape
+    @scrapes = @scrape.all
+    redirect_to get_cities_areas
+  end
 end
