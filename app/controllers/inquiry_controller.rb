b26545e8eb7f6028293973bf4c49cb35690aab37
Seiya-wasabi <se.sato614@gmail.com>
2021-09-24 14:03:38 +0000

    astra

--- app/controllers/inquiry_controller.rb
+++ app/controllers/inquiry_controller.rb
@@ -17,6 +17,12 @@ class InquiryController < ApplicationController
       render :action => 'index'
     end
   end
+  
+  def create
+    @scrape = Area.new.scrape
+    @scrapes = @scrape.all
+    redirect_to get_cities_areas
+  end
 
   def thanks
     # メール送信
