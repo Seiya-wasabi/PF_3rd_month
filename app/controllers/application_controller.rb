5dd0b9953d859796813bc7bacd94e2fdd7016c51
Seiya-wasabi <se.sato614@gmail.com>
2021-10-01 14:39:49 +0000

    text

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,11 @@ class ApplicationController < ActionController::Base
         user_lists_path
     end
   end
+  def index
+    # 入力画面を表示
+    @inquiry = Inquiry.new
+    render :action => 'index'
+  end
 
   protected
 
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,10 +1,6 @@
 class HomesController < ApplicationController
   def top
   end
-  def index
-    # 入力画面を表示
-    @inquiry = Inquiry.new
-    render :action => 'index'
-  end
+  
   
 end
