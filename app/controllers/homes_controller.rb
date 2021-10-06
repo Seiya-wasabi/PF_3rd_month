e46c8dd3ca73447d7cf2bb71c611ba719addf354
Seiya-wasabi <se.sato614@gmail.com>
2021-10-06 14:46:18 +0000

    doge

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,12 +1,5 @@
 class HomesController < ApplicationController
   def top
   end
-  def thanks
-    # メール送信
-    @inquiry = Inquiry.new(params[:inquiry].permit(:name, :email, :message))    
-    InquiryMailer.received_email(@inquiry).deliver
-
-    # 完了画面を表示
-    render :action => 'thanks'
-  end
+  
 end
--- app/controllers/real_properties_controller.rb
+++ app/controllers/real_properties_controller.rb
@@ -7,6 +7,12 @@ class RealPropertiesController < ApplicationController
     @area = Area.new
     @cities = City.where(prefecture_id:0)
   end
+  
+  def create
+    @scrape = Area.new.scrape
+    @scrapes = @scrape.all
+    redirect_to get_cities_areas
+  end
 
 
   def create
