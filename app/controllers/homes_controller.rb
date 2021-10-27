4d5638a6e4c7b7104b14f26d4aaef51521fbb431
Seiya-wasabi <se.sato614@gmail.com>
2021-10-27 14:48:30 +0000

    kind

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,9 +1,14 @@
 class HomesController < ApplicationController
   def top
   end
-  def index
-    # 入力画面を表示
-    @inquiry = Inquiry.new
-    render :action => 'index'
+
+
+def thanks
+    # メール送信
+    @inquiry = Inquiry.new(params[:inquiry].permit(:name, :email, :message))    
+    InquiryMailer.received_email(@inquiry).deliver
+
+    # 完了画面を表示
+    render :action => 'thanks'
   end
 end
